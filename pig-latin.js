ea88fef37aec61514da688142b0916ea6cf6674d
Jonathasdp <jonathas.dp@gmail.com>
2021-09-11 18:17:02 -0300

    pig-latin - solution

---
+++ pig-latin.js
@@ -0,0 +1,7 @@
+function translatePigLatin(str) {
+  return /^[aeiou]/.test(str)
+    ? str.concat("way")
+    : str.replace(/([^aeiou]+)(\w*)/, "$2$1ay");
+}
+
+console.log(translatePigLatin("paragraphs"));
